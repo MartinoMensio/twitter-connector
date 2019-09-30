ab04e6a83a0c0feddad65e3ab7c339fe6b8da9de
Martino Mensio <martino.mensio@open.ac.uk>
2019-09-30 14:43:29 +0100

    description of the profile and using string ids

--- app/model/classes.py
+++ app/model/classes.py
@@ -3,14 +3,15 @@ from pydantic import BaseModel, UrlStr
 
 class User(BaseModel):
     screen_name: str
-    id: int
+    id: str
     image: str # some profiles can have an empty string (temporarily unavailable because it violates the Twitter Media Policy)
     image_full: str
+    description: str
 
 # https://pydantic-docs.helpmanual.io/#postponed-annotations for recursive model
 ref = ForwardRef('Tweet')
 class Tweet(BaseModel):
-    id: int
+    id: str # serializing to string because otherwise the long id gets messed up
     text: str
     retweet: bool
     retweet_source_tweet: Optional[ref]# the ID of the tweet that has been retweeted (retweets with comments instead will have a http://t.co/ link)
